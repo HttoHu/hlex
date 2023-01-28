865a0dfa5a055cbf13e5e4d5c2ccea72b359e5cb
Htto Hu <huyuantao@outlook.com>
2023-01-28 23:07:51 +0800

    update command line usage!

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,32 @@
 #include <unordered_map>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 namespace Utils
 {
     std::string read_file(const std::string &filename);
 }
-
+char buf[1024];
 void gen_lexer(int argc, char **argv)
 {
     using namespace Alg;
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cerr << "invalid argument, usage: hlex [output]\n";
+        std::cerr << "invalid argument, usage: hlex [input] [output]\n";
         exit(1);
     }
+    auto dir = std::filesystem::canonical(std::filesystem::path(argv[0])).parent_path().c_str();
+    sprintf(buf, "%S", dir);
+    std::string exe_path = buf;
 
-    std::string file = Utils::read_file("./rules.hlex");
+    std::string file = Utils::read_file(argv[1]);
     Lexer::Scanner scan(file);
     Lexer::LexerGenerator L(scan);
 
     std::ofstream ofs(argv[1]);
-    ofs << L.gen_code("./template/template.cpp");
+    ofs << L.gen_code(exe_path+"/hlex/template.cpp");
     ofs.close();
 }
 void lex()
@@ -50,7 +54,7 @@ int main(int argc, char **argv)
     try
     {
         // RE();
-        gen_lexer(argc,argv);
+        gen_lexer(argc, argv);
         // lex();
     }
     catch (std::exception &e)
